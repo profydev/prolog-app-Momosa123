2d9be39481e3a4525a8a8bc36fd004f8a3645f9b
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-06-02 09:34:43 +0200

    edit support button to be a link and send support mail

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -79,11 +79,12 @@ export function SidebarNavigation() {
             ))}
           </ul>
           <ul className={styles.list}>
-            <MenuItemButton
+            <MenuItemLink
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              isActive={false}
+              href="mailto:support@prolog-app.com?subject=Support Request: "
             />
             <MenuItemButton
               text="Collapse"
