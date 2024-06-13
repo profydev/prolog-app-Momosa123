93e9f61a26c74c6b663855cb3e531c3804289b26
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-06-13 16:47:07 +0200

    edit footer component

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,19 +1,34 @@
 import styles from "./footer.module.scss";
 import pjson from "../../../package.json";
-export function Footer() {
+import classNames from "classnames";
+import Link from "next/link";
+type Props = {
+  footerClassName?: string;
+};
+export function Footer({ footerClassName }: Props) {
   return (
-    <div className={styles.footer}>
-      <div>
-        <p className={styles.version}>version: {pjson.version}</p>
+    <footer className={classNames(styles.footer, footerClassName)}>
+      <ul className={styles.linkContainer}>
+        <li>
+          <Link href="#">Docs</Link>
+        </li>
+        <li>
+          <Link href="#">API</Link>
+        </li>
+        <li>
+          <Link href="#">Help</Link>
+        </li>
+        <li>
+          <Link href="#">Community</Link>
+        </li>
+      </ul>
+      <div className={styles.logoContainer}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/logo-small.svg" alt="logo" />
       </div>
-      <div className={styles.linkContainer}>
-        <a href="#">Docs</a>
-        <a href="#">API</a>
-        <a href="#">Help</a>
-        <a href="#">Community</a>
+      <div className={styles.version}>
+        <p>version: {pjson.version}</p>
       </div>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/icons/logo-small.svg" alt="logo" className={styles.logo} />
-    </div>
+    </footer>
   );
 }
