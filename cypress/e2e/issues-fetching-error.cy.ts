48b674fa8f739dc881e3ddd39c8fabedadbcaba5
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-06-18 18:48:27 +0200

    fetching error tests

---
+++ cypress/e2e/issues-fetching-error.cy.ts
@@ -0,0 +1,26 @@
+describe("Project List Error Screen", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+      fixture: "projects.json",
+    });
+    cy.intercept("GET", "https://prolog-api.profy.dev/issue?page=1", {
+      forceNetworkError: true,
+    }).as("getIssues");
+
+    cy.visit("http://localhost:3000/dashboard/issues");
+    cy.wait(10000);
+  });
+
+  it("displays an error message when the request fails", () => {
+    cy.get('[data-cy="fetching-error"]')
+      .should("be.visible")
+      .contains("There was a problem while loading");
+  });
+
+  it("reloads the data when the 'Try again' button is clicked", () => {
+    cy.get("button").contains("Try again").click();
+    cy.wait("@getIssues");
+    cy.get('[data-cy="loading-indicator"]').should("be.visible");
+    // Check the projects are loaded here
+  });
+});
